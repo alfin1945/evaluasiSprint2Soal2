5b8a4c1eba68450e90c3f48354e12062e10e70e5
alfin1945 <alfinmardani7@gmail.com>
2020-09-12 07:24:25 +0700

    tambahkan file soal2

---
+++ pelajaran2.js
@@ -0,0 +1,7 @@
+var listDesa1 = ['argodadi,sedayu', 'argomulyo,sedayu', 'argorejo,sedayu', 'argosari', 'bangunjiwo,kasihan', 'banguntapan,banguntapan', 'bantul,bantul', 'baturetno,banguntapan', 'bawuran,pleret', 'canden,jetis', 'caturharjo,pandak', 'cetan srigandi']
+var banyakNilai = listDesa1.length
+
+for (listDesa = 1; listDesa <= banyakNilai; listDesa++) {
+    console.log(listDesa + '.' + listDesa1[listDesa])
+
+}
